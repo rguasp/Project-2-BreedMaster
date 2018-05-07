9877812dfc5e544bb04ff18761fc62e4728786c2
Rafael Guasp <ralfyguasp@gmail.com>
2018-05-07 10:22:25 -0400

    first commit signup

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ const favicon      = require('serve-favicon');
 const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
+const bcrypt        = require("bcrypt");
+const passport      = require("passport");
+const session       = require("express-session");
+const LocalStrategy = require("passport-local").Strategy;
+const flash         = require("connect-flash");
 const path         = require('path');
+const User          = require('./models/user');
+
+const app = express();
 
 
 mongoose.Promise = Promise;
@@ -22,7 +30,6 @@ mongoose
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
-const app = express();
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -49,10 +56,47 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+app.use(flash());
+//passport config area
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
 
 
+//
+
+app.use(passport.initialize());
+app.use(passport.session());
 const index = require('./routes/index');
 app.use('/', index);
+
+const authRouteVariableThing = require('./routes/auth-routes')
+app.use('/', authRouteVariableThing);
 
 
 module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,22 @@
+
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  email: String,
+  location: String,
+  dogtypes: String,
+  role: {
+  type: String,
+  enum : ['GUEST', 'EDITOR', 'ADMIN'],
+  default : 'GUEST'
+}
+}, {
+  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
